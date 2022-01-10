285b571bd99f40e6f6874689a9c2cb349382bbd1
Karol Barroso <karolbarroso5@gmail.com>
2022-01-10 11:47:28 -0800

    Desafío: Menú e-commerce
    Navbar and css styles

---
+++ karol-app/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function NavBar(){
+    return (
+            <>
+            
+            <ul>
+  <li><a href="brand">YouManga</a></li>
+  <li><a href="products">Products</a></li>
+  <li><a href="contact">Contact</a></li>
+  <li><a href="about">About</a></li>
+</ul>
+            
+            </>
+
+    )
+
+
+}
