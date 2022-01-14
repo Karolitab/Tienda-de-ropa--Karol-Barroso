31aef38686880c68b63869248fa2deeead878ea0
Karol Barroso <karolbarroso5@gmail.com>
2022-01-13 21:56:01 -0800

    Created CarWidget and ItemListContainer

--- karol-app/src/App.js
+++ karol-app/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+// import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <>
     <NavBar />
+  
+  
     </>
 
   );
--- karol-app/src/components/ItemListContainer/ItemListContainer.js
+++ karol-app/src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,3 @@
 import react from 'react';
 
-export default function itemListContainer (){
-
-}
+//export default function itemListContainer (){}
