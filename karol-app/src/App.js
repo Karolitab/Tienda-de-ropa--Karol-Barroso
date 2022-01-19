d6923225b127e457ef27f38dd102945f29fbaa94
Karol Barroso <karolbarroso5@gmail.com>
2022-01-18 22:40:41 -0800

    Counter added

--- karol-app/src/App.js
+++ karol-app/src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 // import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Counter from './components/Counter/Counter';
 
 function App() {
   return (
     <>
     <NavBar />
-  
+    <Counter />
   
     </>
 
